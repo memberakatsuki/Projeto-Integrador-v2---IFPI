a75871431efd7f6181cae5bc91c623ad51b14800
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-23 13:54:49 -0300

    Adciona Pool Client Pg

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ module.exports = {
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
+  port: process.env.PORT,
   define: {
     timestamps: true,
     underscored: true,
