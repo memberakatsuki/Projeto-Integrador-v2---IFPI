655ad9416c53681580834135823f980de7af8bb2
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-23 09:50:53 -0300

    adciona Procfile

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DATABASE_URL,
   define: {
     timestamps: true,
     underscored: true,
