88aced311d7da6205d8cf130600e5e3a9aef0b7d
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-23 09:43:02 -0300

    adciona ENV

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,11 @@
 module.exports = {
-  production:{
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    define: {
-      timestamps: true,
-      underscored: true,
-    },
-  }
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
 };
