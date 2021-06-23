2f59d9d3c0978edff3f64c1ad888c3c4777bc9d8
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-23 14:42:21 -0300

    Alteração database

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,19 @@
 {
-  "development": {
+  "development" = {
     "username": "root",
     "password": null,
     "database": "database_development",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "test": {
+  "test" = {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "production": {
+  "production" = {
     "use_env_variable": "DATABASE_URL"
   }
 }
