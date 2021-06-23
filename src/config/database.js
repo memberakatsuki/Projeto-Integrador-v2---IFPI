c5aa01777251a2c5e147e30b3a1d7dc69add84c8
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-23 13:08:59 -0300

    Adciona Pool Client Pg

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
+  host: process.env.,
   username: "postgres",
   password: "sdev18010",
   database: process.env.DATABASE_URL,
--- src/http.js
+++ src/http.js
@@ -6,6 +6,15 @@ const { Server } = require("socket.io")
 const cors = require("cors")
 require("dotenv").config()
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
 require("./database")
 
 const app = express()
@@ -24,6 +33,19 @@ app.get("/client", (req,res) =>{
 
 app.get("/admin", (req,res) =>{
   return res.render("html/admin.html")
+})
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 })
 
 /* app.get("/pages/register", (req,res) =>{
