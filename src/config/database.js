847fc7a139f6387385ef1a259ee2eff5576ca87f
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-23 21:02:48 -0300

    procfile

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,23 @@
+'use strict'
+
+const { Sequelize } = require("sequelize")
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions:{
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+})
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully."))
+  .catch((err) => console.log("Unable to connect to the database:", err))
+
+
+module.exports = sequelize
+
 /* module.exports = {
   dialect: 'postgres',
   host: 'localhost',
--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,25 +1,9 @@
-'use strict'
-
-const { Sequelize } = require("sequelize")
-//const config  = require("../config/database")
+//const { Sequelize } = require("sequelize")
+const sequelize   = require("../config/database")
 const Settings = require("../models/Setting")
 const Users = require("../models/User")
 const Messages = require("../models/Message")
 const Connections = require("../models/Connection")
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions:{
-    ssl: {
-      rejectUnauthorized: false
-    }
-  }
-})
-
-sequelize
-  .authenticate()
-  .then(() => console.log("Connection has been established successfully."))
-  .catch((err) => console.log("Unable to connect to the database:", err))
-
 
 
 Settings.init(sequelize)
