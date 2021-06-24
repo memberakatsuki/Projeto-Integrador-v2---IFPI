15dbccda0bae9c37b3563bbe71022ed7852b6a07
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-24 11:40:03 -0300

    app finish

--- src/server.js
+++ src/server.js
@@ -2,6 +2,4 @@ const  { http }   = require("./http")
 require("./websocket/client")
 require("./websocket/admin")
 
-http.listen( process.env.PORT || 3333, () => {
-  console.log("server running on port 3333!")
-})
+http.listen( process.env.PORT || 3333 )
