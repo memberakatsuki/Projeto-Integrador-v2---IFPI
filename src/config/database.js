1fb5d89e5deaa7e2b43a10b21e54358267908eb8
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-23 21:19:23 -0300

    procfile

--- src/config/database.js
+++ src/config/database.js
@@ -10,12 +10,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   }
 })
 
-sequelize
-  .authenticate()
-  .then(() => console.log("Connection has been established successfully."))
-  .catch((err) => console.log("Unable to connect to the database:", err))
-
-
 module.exports = sequelize
 
 /* module.exports = {
