fb497f084687006e138a6a7695f1e0c951a57618
memberakatsuki <allysonsousaplay123@gmail.com>
2021-06-24 11:37:36 -0300

    app finish

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,26 @@
 const Sequelize = require("sequelize")
-const config   = require("../config/database")
+//const config   = require("../config/database")
 const Setting = require("../models/Setting")
 const User = require("../models/User")
 const Message = require("../models/Message")
 const Connection = require("../models/Connection")
 
-const connection = new Sequelize(config)
+const connection = new Sequelize({
+  dialect: process.env.DB_DIALECT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  dialectOptions:{
+    ssl:{
+      rejectUnauthorized: process.env.DB_SSL
+    }
+  },
+  define:{
+    timestamps: true,
+    underscored: true
+  }
+})
 
 connection
   .authenticate()
